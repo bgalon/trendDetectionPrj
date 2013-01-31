26d752bb721da18b9d4be5aa537c135e817156b9
ekravi <ekravi@lap198.cs.technion.ac.il>
2013-01-31 12:28:13 +0200

    a

--- filterOSM/src/config/Configurtaion.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public final class Configurtaion {
-	public static final String OSM_FILE = "osmFiles/london_small.osm";
-	public static final String TEST_FILE = "osmFiles/test.osm";
-
-}
